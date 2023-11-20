f7246587fd48951c5843324fbdc96319c9923e04
Daniel Torres <danieljlt23@gmail.com>
2023-11-20 20:38:24 -0300

    criaçãod do controller dos pop ups

---
+++ src/main/java/com/example/projeto/PopupsDeCompraController.java
@@ -0,0 +1,38 @@
+package com.example.projeto;
+
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PopupsDeCompraController{
+
+    @FXML
+    private ComboBox caixaDeEscolha;
+
+    @FXML
+    private Button confirm;
+
+    @FXML
+    void confirmarCompra(ActionEvent event) {
+
+    }
+
+    @FXML
+    void select(ActionEvent event) {
+
+    }
+
+    public void initialize(URL url, ResourceBundle rb) {
+        ObservableList<String> list = FXCollections.observableArrayList("Cartão de crédito ou débito", "Boleto", "Pix", "Dinheiro");
+        caixaDeEscolha.setItems(list);
+    }
+
+}
+
