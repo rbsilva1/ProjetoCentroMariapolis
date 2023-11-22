dd725f49f1ac5ea903edc9bfb5ae2c2961ba4231
Daniel Torres <danieljlt23@gmail.com>
2023-11-22 20:52:42 -0300

    retirado os botões dede voltar

--- src/main/java/com/example/projeto/CodigoInvalidoController.java
+++ src/main/java/com/example/projeto/CodigoInvalidoController.java
@@ -2,16 +2,35 @@ package com.example.projeto;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CodigoInvalidoController {
 
     @FXML
     private Button voltar;
 
-    @FXML
-    void onClick(ActionEvent event) {
+    private Stage stage;
+    private Scene scene;
 
+    @FXML
+    void onClick(ActionEvent event) throws IOException {
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
+        Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root, bounds.getWidth(), bounds.getHeight());
+        stage.setMaximized(true);
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
