58592cbfe03a424706dc88410dc1c9cc43d9621b
andersoncp123 <andersoncp321@gmail.com>
2023-12-10 21:51:52 -0300

    Atualizando HomeController para a versão mais recente

--- src/main/java/com/example/projeto/HomeController.java
+++ src/main/java/com/example/projeto/HomeController.java
@@ -3,16 +3,8 @@ package com.example.projeto;
 import com.example.projeto.validacoes.Formatar_CPF;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Rectangle2D;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.stage.Screen;
-import javafx.stage.Stage;
-import javafx.stage.Modality;
 import java.io.IOException;
 
 import static com.example.projeto.validacoes.CPF_Validacao.validarCPF;
@@ -40,55 +32,24 @@ public class HomeController {
     @FXML
     private Button paginainicial;
 
-    private Scene scene;
-    private Stage stage;
-
     @FXML
     public void abrirHistorico(ActionEvent event) throws IOException {
-        Screen screen = Screen.getPrimary();
-        Rectangle2D bounds = screen.getVisualBounds();
-        Parent root = FXMLLoader.load(getClass().getResource("Historico.fxml"));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root, bounds.getWidth(), bounds.getHeight());
-        stage.setMaximized(true);
-        stage.setScene(scene);
-        stage.show();
+        AbrirFXML.abrirFXML(event, "Historico.fxml");
     }
 
     @FXML
     public void abrirInscricoes(ActionEvent event) throws IOException {
-        Screen screen = Screen.getPrimary();
-        Rectangle2D bounds = screen.getVisualBounds();
-        Parent root = FXMLLoader.load(getClass().getResource("Inscricoes.fxml"));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root, bounds.getWidth(), bounds.getHeight());
-        stage.setMaximized(true);
-        stage.setScene(scene);
-        stage.show();
+        AbrirFXML.abrirFXML(event, "Inscricoes.fxml");
     }
 
     @FXML
     public void voltarTelaInicial(ActionEvent event) throws IOException {
-        Screen screen = Screen.getPrimary();
-        Rectangle2D bounds = screen.getVisualBounds();
-        Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root, bounds.getWidth(), bounds.getHeight());
-        stage.setMaximized(true);
-        stage.setScene(scene);
-        stage.show();
+        AbrirFXML.abrirFXML(event, "Home.fxml");
     }
 
     @FXML
     public void paginaEventos(ActionEvent event) throws IOException {
-        Screen screen = Screen.getPrimary();
-        Rectangle2D bounds = screen.getVisualBounds();
-        Parent root = FXMLLoader.load(getClass().getResource("Eventos.fxml"));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root, bounds.getWidth(), bounds.getHeight());
-        stage.setMaximized(true);
-        stage.setScene(scene);
-        stage.show();
+        AbrirFXML.abrirFXML(event, "Eventos.fxml");
     }
 
     @FXML
@@ -98,14 +59,7 @@ public class HomeController {
         if (cpfValido) {
             // Se o CPF for válido chamar as funções necessárias!
         } else {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("PopupErroCpf.fxml"));
-            Parent root = loader.load();
-            Stage popupStage = new Stage();
-            popupStage.initModality(Modality.APPLICATION_MODAL);
-            popupStage.setTitle("Erro CPF !");
-            popupStage.setScene(new Scene(root));
-            popupStage.setResizable(false);
-            popupStage.show();
+            AbrirPopup.abrirPopup(event, "PopupErroCpf.fxml", "Erro CPF !");
         }
     }
 
