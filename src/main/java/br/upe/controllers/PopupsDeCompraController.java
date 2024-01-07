a2997c45a02feeed9e6639dc8e0f3dc94af9f352
rbsilva1 <rodrigo.belarmino17@gmail.com>
2024-01-07 11:45:14 -0300

    merge organizar-telas into main

--- src/main/java/br/upe/controllers/PopupsDeCompraController.java
+++ src/main/java/br/upe/controllers/PopupsDeCompraController.java
@@ -6,7 +6,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,9 +13,8 @@ import java.util.ResourceBundle;
 import br.upe.utils.AbrirPopup;
 
 public class PopupsDeCompraController implements javafx.fxml.Initializable {
-
     @FXML
-    private ComboBox caixaDeEscolha;
+    private ComboBox<String> caixaDeEscolha;
 
     @FXML
     private Button confirm;
@@ -36,6 +34,5 @@ public class PopupsDeCompraController implements javafx.fxml.Initializable {
         ObservableList<String> list = FXCollections.observableArrayList("Cartão de crédito ou débito", "Boleto", "Pix", "Dinheiro");
         caixaDeEscolha.setItems(list);
     }
-
 }
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module br.upe {
     requires transitive javafx.graphics;
-    requires javafx.controls;
+    requires transitive javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
 
