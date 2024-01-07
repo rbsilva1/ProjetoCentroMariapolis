d0249476754d8f331725e12bc980e5b89cd20cc5
rbsilva1 <rodrigo.belarmino17@gmail.com>
2024-01-07 13:54:59 -0300

    adicionar testes de validacao de CPF

--- src/test/java/ValidarCPFTest.java
+++ src/test/java/ValidarCPFTest.java
@@ -7,14 +7,26 @@ import static br.upe.utils.CPF_Validacao.validarCPF;
 
 public class ValidarCPFTest {
   @Test
-  @DisplayName("Teste de CPF válido")
+  @DisplayName("Deve retornar verdadeiro para CPF válido")
   public void testarCPFValido() {
     assertTrue(validarCPF("116.706.480-10"));
   }
 
   @Test
-  @DisplayName("Teste de CPF inválido")
+  @DisplayName("Deve retornar falso para CPF inválido")
   public void testarCPFInvalido() {
     assertFalse(validarCPF("116.706.480-11"));
   }  
+
+  @Test
+  @DisplayName("Deve retornar falso para CPF com menos de 11 dígitos")
+  public void testarCPFMenosDigitos() {
+    assertFalse(validarCPF("116.706.480-1"));
+  }
+
+  @Test
+  @DisplayName("Deve retornar falso para CPF 11 dígitos iguais")
+  public void testarCPF11DigitosIguais() {
+    assertFalse(validarCPF("111.111.111-11"));
+  }
 }
